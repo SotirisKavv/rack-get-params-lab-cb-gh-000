3887d5f1a65d615279e2b2a18e1a108bad2ecc73
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-22 00:49:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,8 +24,9 @@ class Application
       end
     elsif req.path.match(/add/)
       search_term = req.params["item"]
-      if !@@items.include?(search_term)
+      if @@items.include?(search_term)
         @@cart << search_term
+        resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
